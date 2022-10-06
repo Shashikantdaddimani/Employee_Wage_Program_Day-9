741ca4a3b5f2778fc63dc4dca612b667caa73b82
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-06 17:31:32 +0530

    Added Employee wage program display message

---
+++ src/com/emp/wage/EmployeeWageProgram.java
@@ -0,0 +1,10 @@
+package com.emp.wage;
+
+public class EmployeeWageProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Employee wage Computation Program");
+
+	}
+
+}
