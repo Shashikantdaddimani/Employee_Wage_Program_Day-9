6c659f7d5133eca991d8049272fefeb8d84a338e
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-06 17:44:16 +0530

    Added the Employee present or absent program

--- src/com/emp/wage/EmployeeWageProgram.java
+++ src/com/emp/wage/EmployeeWageProgram.java
@@ -1,10 +1,24 @@
 package com.emp.wage;
 
+import java.util.Random;
+
 public class EmployeeWageProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee wage Computation Program");
-
+		
+		Random random = new Random();
+		int randomNum = random.nextInt(2);
+		
+		if (randomNum==0) 
+		{
+			System.out.println("Emloyee is Present");
+			
+		}
+		else
+		{
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
