a78fb642137265f1d0c7c5525e2c4b94d6fa01fb
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-10-09 23:14:39 +0530

    Added Ability to compute Employee Wage for multiple companies

--- src/com/emp/wage/EmployeeWageProgram.java
+++ src/com/emp/wage/EmployeeWageProgram.java
@@ -4,54 +4,72 @@ import java.util.*;
 
 public class EmployeeWageProgram {
 
-//	public static void main(String[] args) {
-//		System.out.println("Welcome to the Employee wage Computation Program");
-		
-		
-		static int wagePerHour=20;
-		static int fullDayHours=8;
-		static int halfDayHours=4;
-		static int salary = 0;
-		static int monthlySalary = 0;
-		static int monthlyWorkingDays = 20;
-		static int days = 0;
-		static int monthlyHours = 0;
-		 
-		static void computeEmpwage() {
-			
-			Random random = new Random();
-		
-		    while(days!=20 && monthlyHours!=100) {
-			 
-		    	days++;
-			 int randomNum = random.nextInt(3);
-		
-		switch(randomNum)
-		{
-		case 0:
-			System.out.println("Employee is Absent");
-			break;
-		
-		case 1:
-			System.out.println("Employee is working fullday");
-			salary = wagePerHour * fullDayHours;
-			monthlyHours = monthlyHours + fullDayHours;
-			break;
-		
-		case 2:
-			System.out.println("Employee is working Halfday");
-			salary = wagePerHour * halfDayHours;
-			monthlyHours = monthlyHours + halfDayHours;
-		}
-		
-		monthlySalary = monthlySalary +salary;
-		System.out.println("Days"+ days + "Salary is: "+salary);
+	public static final int Half_Day_Hours = 4;
+	public static final int Full_Day_Hours = 4;
+	
+	private final String companyName;
+	private final int empWagePerHour;
+	private final int workingHours;
+	private final int workingDays;
+	
+	static Random random = new Random();
+	public EmployeeWageProgram(String companyName, int empWagePerHour, int workingHours, int workingDays) {
+		this.companyName = companyName;
+		this.empWagePerHour = empWagePerHour;
+		this.workingHours = workingHours;
+		this.workingDays = workingDays;
 	}
-		 System.out.println("Monthly Salary: "+monthlySalary);
-}
-		  void displaymonthlySalary() {
-			System.out.println("Monthly salary: "+monthlySalary);
-		}
-}
 	
+	public int calculateWage() {
+		int salary = 0;
+		int monthlySalary =0;
+		int monthlyWorkingHours = 0;
+		int days = 0;
+		
+		while (monthlyWorkingHours <= workingHours && days <= workingDays) {
+			
+			days++;
+			
+			int empCheck = random.nextInt(3);
+			
+			switch (empCheck) {
+			case 1:
+					System.out.println("Employee is working fullday");
+					salary = empWagePerHour * Full_Day_Hours;
+					monthlyWorkingHours = monthlyWorkingHours + Full_Day_Hours;
+					break;
+					
+			case 2:
+					System.out.println("Employee is working Halfday");
+					salary = empWagePerHour * Half_Day_Hours;
+					monthlyWorkingHours = monthlyWorkingHours + Half_Day_Hours;	
+					break;
+					
+			default :
+			        
+			        System.out.println("Employee is Absent");
+						
+			}
+			       monthlySalary = monthlySalary + salary;
+			       System.out.println("Days"+ days + "Salary is: "+salary);
+			       System.out.println("Monthly Salary: "+monthlySalary);
+					
+		    }
+		
+		
+	              return monthlyWorkingHours * empWagePerHour;
+	        }
+				public static void main(String[] args) { 
+				   // EmployeeWageProgram Bajaj = new  EmployeeWageProgram( companyName, empWagePerHour, workingHours, workingDays);
+					EmployeeWageProgram  Bajaj = new EmployeeWageProgram("Bajaj", 40,60,21);
+				    EmployeeWageProgram  Tvs = new EmployeeWageProgram("Tvs", 40,60,21);
+				    
+				    System.out.println("Total employee wage of Company: " +Bajaj.companyName + ":"+Bajaj.calculateWage());
+				    System.out.println("Total employee wage of Company: " +Tvs.companyName + ":"+Tvs.calculateWage());
+	       }
+
 	
+}
+
+		
+		
